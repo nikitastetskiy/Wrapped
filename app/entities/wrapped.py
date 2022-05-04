5dc0c5de668e1350925e10660c9fb358ca393825
Nikita Stetskiy <nikin929@gmail.com>
2022-05-04 17:28:26 +0900

    Retoco de nuevo la clase wrapped #4

--- app/entities/wrapped.py
+++ app/entities/wrapped.py
@@ -1,11 +1,12 @@
 """ This module contains wrapped entity """
-
 from dataclasses import dataclass
 from typing import Optional
 
+
 @dataclass
 class Wrapped:
-    """ Represents a wrapped trend that you could be informed on the platform """
+    """ Represents a wrapped trend that you could
+     be informed on the platform """
     title: str
     summary: str
     image: Optional[str] = None
